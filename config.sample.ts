acd369f7e6d1c30669fa4f6bb97797cc237e76e7
xxuxa-k <xyz@tk-tech.xyz>
2025-08-23 13:32:53 +0900

    add graphql sample

--- config.sample.ts
+++ config.sample.ts
@@ -1,3 +1,5 @@
+export const SALESFORCE_API_VERSION="v64.0"
+
 export default {
   issuer: "", // Consumer Key from Salesforce Connected App
   audience: "https://login.salesforce.com", // or https://test.salesforce.com
---
+++ graphql.ts
@@ -0,0 +1,69 @@
+import { z } from "zod"
+import { getJsforceConnection } from "./jsforceConnection"
+import { SALESFORCE_API_VERSION } from "./config"
+import { readFile } from "fs/promises"
+
+const ReponseSchema = z.object({
+  data: z.object({
+    uiapi: z.object({
+      query: z.object({
+        Account: z.object({
+          edges: z.array(
+            z.object({
+              node: z.object({
+                InvalidProperty: z.string().optional().describe("Does not cause an error."),
+                Id: z.string(),
+                Name: z.object({
+                  value: z.string(),
+                }),
+                Website: z.object({
+                  value: z.string().nullable(),
+                }),
+              })
+            })),
+        }),
+      }),
+    })
+  }),
+  errors: z.array(z.any()),
+})
+
+const main = async function () {
+  const conn = await getJsforceConnection()
+
+  const buffer = await readFile("./query.graphql")
+  const query = buffer.toString()
+
+  const url = new URL(`${conn.instanceUrl}/services/data/${SALESFORCE_API_VERSION!}/graphql`)
+  const response = await fetch(url, {
+    method: "post",
+    headers: new Headers({
+      "Authorization": `Bearer ${conn.accessToken}`,
+      "Content-Type": "application/json",
+    }),
+    body: JSON.stringify({
+      query,
+      variables: {
+        keyword: "東京都",
+      },
+    }),
+  })
+
+  const json = await response.json()
+  if (!response.ok) {
+    console.error("Error:", json)
+    throw new Error("Error")
+  }
+  const { success, data, error } = ReponseSchema.safeParse(json)
+
+  if (!success) {
+    console.error(error)
+    throw new Error("Invalid response format")
+  }
+  data.data.uiapi.query.Account.edges.map(edge => {
+    const node = edge.node
+    console.log(node.Id, node.Name.value, node.Website.value)
+  })
+}
+
+main()
